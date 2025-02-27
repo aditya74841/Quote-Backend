27f64d3b4df49e3a3db815f206326b95b45e9c7f
Aditya Ranjan <aditya74810@gmail.com>
2025-02-27 14:29:47 +0530

    changes quote controller

--- src/controller/quote.controller.js
+++ src/controller/quote.controller.js
@@ -5,9 +5,8 @@ import cron from "node-cron";
 import axios from "axios";
 
 // Function to fetch and store the daily quote
-const storeQuote = async () => {
+const storeQuote = async (req, res) => {
   try {
-    console.log("checking the line 10");
     // Get today's date in YYYY-MM-DD format
     const today = new Date().toISOString().split("T")[0];
 
@@ -19,10 +18,11 @@ const storeQuote = async () => {
       },
     });
 
-
     if (existingQuote) {
-      console.log("A quote for today already exists. Skipping...");
-      return;
+     
+      res
+        .status(404)
+        .json({ message: "A quote for today already exists. Skipping..." });
     }
 
     // Fetch the quote from ZenQuotes API
@@ -39,8 +39,7 @@ const storeQuote = async () => {
     // Create and save the new quote
     const newQuote = new Quote({ quote, author });
     await newQuote.save();
-
-    console.log("Quote stored successfully:", quote);
+    return res.status(201).json({ message: "Quote saved successfully" });
   } catch (error) {
     console.error("Error fetching or storing the quote:", error);
   }
@@ -52,11 +51,10 @@ const storeQuote = async () => {
 //   await storeQuote();
 // });
 
-
-cron.schedule("1 0 * * *", async () => {
-  console.log("Fetching new daily quote...");
-  await storeQuote();
-});
+// cron.schedule("1 0 * * *", async () => {
+//   console.log("Fetching new daily quote...");
+//   await storeQuote();
+// });
 const getTodayQuote = async (req, res) => {
   try {
     // Get today's date in YYYY-MM-DD format
@@ -90,7 +88,9 @@ const getAllQuotes = async (req, res, next) => {
     today.setHours(0, 0, 0, 0);
 
     // Fetch all quotes except today, sorted from newest to oldest
-    const quotes = await Quote.find({ createdAt: { $lt: today } }).sort({ createdAt: -1 });
+    const quotes = await Quote.find({ createdAt: { $lt: today } }).sort({
+      createdAt: -1,
+    });
 
     res.status(200).json(quotes);
   } catch (error) {
